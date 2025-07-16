8915f0638cbbf77a7744f941582b25f6bbb9684b
aaronsele <48522418@est.ort.edu.ar>
2025-07-16 08:36:29 -0300

    changes

--- integrador/src/App.jsx
+++ integrador/src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import EventosList from "./pages/EventosList";
+import EventoDetalle from "./pages/EventoDetalle";
+import Form from "./pages/Form";
+import MisEventos from "./pages/MisEventos";
+import UbicacionesList from "./pages/UbicacionesList";
+import Layout from './Layout/LayoutPage';
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem("token");
+};
+
+const PrivateRoute = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="/login" />;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Layout />}>
+        <Route path="eventos" element={<EventosList />} />
+        <Route path="eventos/:id" element={<EventoDetalle />} />
+        <Route path="crear-evento" element={<PrivateRoute><Form/></PrivateRoute>} />
+        <Route path="mis-eventos" element={<PrivateRoute><MisEventos /></PrivateRoute>} />
+        <Route path="ubicaciones" element={<PrivateRoute><UbicacionesList /></PrivateRoute>} />
+        <Route path="*" element={<h1>404 😢</h1>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
